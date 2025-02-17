48153da9076c8028afa4e56ff3afc9e4834aa7b4
Gijsdeman <github@deman.dev>
2025-02-17 15:15:48 +0100

    feat(release): update release
    Remove build files from repository and release using NPM.
    
    BREAKING CHANGE: removes buildfiles; has to be installed through NPM

--- release.config.js
+++ release.config.js
@@ -1,27 +1,9 @@
+import  { releaseConfig } from '@gewis/release-config';
+
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
-  branches: ['main'],
-  plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        successComment: false,
-        failComment: false,
-        failTitle: false,
-        labels: false,
-        releasedLabels: false,
-      },
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['package.json'],
-        message: 'chore(release): ${nextRelease.version}\n\n${nextRelease.notes}',
-      },
-    ],
-  ],
+  branches: ["main"],
+  ...releaseConfig
 };
